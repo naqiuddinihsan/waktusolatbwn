2a109c666bed81891faa63e72315315089c8d0f6
Ihsan <15049683+naqiuddinihsan@users.noreply.github.com>
2026-09-25 07:59:23 +0800

    Update service worker cache version to 3.6.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'waktu-solat-v3.5.0';
+const CACHE_NAME = 'waktu-solat-v3.6.0';
 const ASSETS = [
   './',
   './index.html',
@@ -9,7 +9,7 @@ const ASSETS = [
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-  self.skipWaiting(); // Forces the new service worker to activate immediately
+  self.skipWaiting(); 
 });
 
 self.addEventListener('activate', (e) => {
@@ -17,7 +17,7 @@ self.addEventListener('activate', (e) => {
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
         if (key !== CACHE_NAME) {
-          return caches.delete(key); // Deletes the old buggy cache
+          return caches.delete(key); 
         }
       }));
     })
