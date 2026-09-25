6558191e960e50ac59bcbbe8516d6eda38ebdbf5
Ihsan <15049683+naqiuddinihsan@users.noreply.github.com>
2026-09-25 16:04:41 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 /*
 File Name: sw.js
-Version: 10.0.0
-Description: Network-first service worker to instantly sync the tactile UI and PTR upgrades.
+Version: 11.0.0
+Description: Network-first service worker to instantly sync the UI/UX layout and badge fixes.
 */
 
-const CACHE_NAME = 'waktu-solat-v10.0.0';
+const CACHE_NAME = 'waktu-solat-v11.0.0';
 const ASSETS = [
   './',
   './index.html',
